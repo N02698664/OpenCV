a58a857691569638ea1bb00caf9aec108ad1e6b4
Kevin Anderson <n02698664@hawkmail.newpaltz.edu>
2013-11-14 16:50:01 -0500

    Update monitor.py

--- doc/monitor.py
+++ doc/monitor.py
@@ -1,26 +1,24 @@
+#!/usr/bin/env python
+
 import os
 import pyinotify
 
-wm = pyinotify.WatchManager() #create a watch manager
-mask = pyinotify.IN_CREATE #watched event
+wm = pyinotify.WatchManager()
+mask = pyinotify.IN_CREATE
 
-class CVProcessing(ProcessEvent):
-    def process_IN_CREATE(self, event):
-        execfile("hello_world.py")  #specialized event processor for any new file created
-                                    #in the watched directory
-        
-notifier = pyinotify.Notifier(wm, CVProcessing()) #creates a notification object
+class CVProcessing(pyinotify.ProcessEvent):
+	def process_IN_CREATE(self, event):
+		execfile("/home/pi/hello_world.py")
 
-wdd = wm.add_watch('/home/pi/OpenCV', mask, rec=False) #creates a watch on the OpenCV folder
+notifier = pyinotify.Notifier(wm, CVProcessing(), timeout=10)
 
-while True:  # loop forever
-    try:
-        # process the queue of events, if any
-        notifier.process_events()
-        if notifier.check_events():
-            # read notified events and enqeue them
-            notifier.read_events()
-    except KeyboardInterrupt:
-        # destroy the inotify's instance on this interrupt (stop monitoring)
-        notifier.stop()
-        break
+wm.add_watch('/home/pi/OpenCV/', mask, rec=False)
+
+while True:
+	try:
+		notifier.process_events()
+		if notifier.check_events():
+			notifier.read_events()
+	except KeyboardInterrupt:
+		notifier.stop()
+		break
