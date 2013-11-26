39676747daa07f29888fd68286b42a8122dd36d4
Kevin Anderson <n02698664@hawkmail.newpaltz.edu>
2013-11-26 12:06:47 -0500

    Update process.py

--- code/process.py
+++ code/process.py
@@ -1,24 +1,21 @@
-#!/usr/bin/env python
-
 import os
-import pyinotify
+import numpy as np
+import cv2
 
-wm = pyinotify.WatchManager()
-mask = pyinotify.IN_CREATE
 
-class CVProcessing(pyinotify.ProcessEvent):
-        def process_IN_CREATE(self, event):
-                execfile("/home/pi/process.py")
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_casacde = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-notifier = pyinotify.Notifier(wm, CVProcessing(), timeout=10)
+img = cv2.imread('/home/pi/OpenCV/test.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-wm.add_watch('/home/pi/OpenCV/', mask, rec=False)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+        img = cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 100)
+        eyes_in_face = img[y:y+h, x:x+w]
+        gray_eyes_in_face = img[y:y+h, x:x+w]
+        eyes = eyes_cascade.detectMultiScale(gray_eyes_in_face)
+        for (ex, ey, ew, eh) in eyes:
+                img = cv2.rectangle(eyes_in_face, (ex,ey), (ex+ew,ey+eh), (0,255,0), 100)
 
-while True:
-        try:
-                notifier.process_events()
-                if notifier.check_events():
-                        notifier.read_events()
-        except KeyboardInterrupt:
-                notifier.stop()
-                break
+cv2.imwrite('/home/pi/OpenCV/test_processed.jpg', img)
